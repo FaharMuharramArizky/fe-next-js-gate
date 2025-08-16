2bff5fa33bab14a43bc8577ec3855f63bc5485a5
faharmuharram <146865706+faharmuharram@users.noreply.github.com>
2025-08-16 12:18:43 +0700

    penyesuaian jam

--- app/gate/page.tsx
+++ app/gate/page.tsx
@@ -1,40 +1,52 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { useState, useEffect } from "react";
 
 export default function GatePage() {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState<string>("");
+  const [currentTime, setCurrentTime] = useState<string>("");
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentTime(new Date());
+      const now = new Date();
+      const formattedDate = now.toLocaleDateString("id-ID", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+
+      const formattedTime = now.toLocaleTimeString("id-ID", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+
+      setCurrentDate(`${formattedDate} `)
+      setCurrentTime(` ${formattedTime}`);
     }, 1000);
+
     return () => clearInterval(interval);
   }, []);
 
-  const formattedDate = currentTime.toLocaleString("id-ID", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  const formattedTime = currentTime.toLocaleString("id-ID", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
   return (
     <div className="w-full flex flex-col gap-4">
       <div className="w-full flex flex-row p-4 justify-between">
-        <Image src="/samudera.png" alt="My awesome image" width={300} height={100}></Image>
+        <Image
+          src="/samudera.png"
+          alt="My awesome image"
+          width={300}
+          height={100}
+        />
         <div className="flex items-center text-lg">
-          <p>
-            {formattedDate} <span className="text-red-500">{formattedTime}</span>
+          <p> {currentDate}
+            <span className="text-red-500">{currentTime}</span>
           </p>
         </div>
       </div>
+
       <div className="text-center font-bold text-2xl">
-        <p>GATE OUT RECEIVING / DELIVERY (GATE 02)</p>
+        <p>GATE OUT RECEIVING / DELIVERY - (GATE 02)</p>
       </div>
     </div>
   );
