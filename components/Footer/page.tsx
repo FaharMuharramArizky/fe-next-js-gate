45b856b73c425a2381c7130a2e9c473050a1573f
FaharMuharramArizky <faharmuharramarizky@gmail.com>
2025-08-18 20:38:27 +0700

    Penyesuaian Layout Gate

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-red-500 text-white text-center p-3 fixed bottom-0 left-0 w-full">
+    <div className="bg-red-500 text-white text-center p-3 sticky z-50 bottom-0 left-0 w-full">
       <p>© 2025 PT Pelabuhan Palaran</p>
-    </footer>
+    </div>
   );
 }
